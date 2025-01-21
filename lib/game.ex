14ecfd61399e2dc1490ae7b65b31277bc1f509c9
Tim Beck <becktimd@gmail.com>
2025-01-20 21:37:41 -0800

    deal hands

--- lib/game.ex
+++ lib/game.ex
@@ -1,12 +1,14 @@
 defmodule Briscola.Game do
   alias Briscola.Deck
 
+  @hand_size 3
+
   @type t() :: %Briscola.Game{}
-  defstruct [:deck, :players, :hands, :briscola, :trick, :turn]
+  defstruct [:deck, :players, :hands, :briscola, :trick, :action_on]
 
   @spec new(keyword()) :: Briscola.Game.t()
   def new(opts \\ []) do
-    players = Keyword.get(opts, :player_count, 2)
+    players = Keyword.get(opts, :players, 2)
     hands = Enum.map(1..players, fn _ -> [] end)
 
     {deck, [briscola]} =
@@ -14,15 +16,22 @@ defmodule Briscola.Game do
       |> Deck.shuffle()
       |> Deck.take(1)
 
-    trick = []
+    {deck, hands} = deal(deck, hands)
 
     %Briscola.Game{
       deck: deck,
       players: players,
       hands: hands,
       briscola: briscola,
-      trick: trick
+      trick: [],
+      action_on: Keyword.get(opts, :goes_first, 0)
     }
+  end
+
+  defp deal(deck, hands) do
+    {new_deck, cards} = Deck.take(deck, @hand_size * length(hands))
+    hands = Enum.chunk_every(cards, @hand_size)
+    {new_deck, hands}
   end
 
   @spec trump_suit(t()) :: Briscola.Card.suit()
