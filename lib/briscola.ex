72b1e32259fb685ac6c4d57f322154571c64d2bb
Tim Beck <socaltdb@gmail.com>
2025-01-20 17:51:27 -0800

    decks

--- lib/briscola.ex
+++ lib/briscola.ex
@@ -1,18 +1,45 @@
 defmodule Briscola do
   @moduledoc """
-  Documentation for `Briscola`.
+  `Briscola` card game deck & rules.
   """
+  @suits [
+    :cups,
+    :batons,
+    :coins,
+    :swords
+  ]
 
-  @doc """
-  Hello world.
+  @ranks 0..9
 
-  ## Examples
 
-      iex> Briscola.hello()
-      :world
+  defmodule Card do
+    @type t() :: %Card{}
+    defstruct [:suit, :rank]
+  end
 
-  """
-  def hello do
-    :world
+  defmodule Deck do
+    @type t() :: %Deck{}
+    defstruct [:cards]
+  end
+
+  @spec new_deck() :: Deck.t()
+  def new_deck() do
+    cards =
+      for suit <- @suits,
+          rank <- @ranks,
+          do: %Card{suit: suit, rank: rank}
+
+    %Deck{cards: cards}
+  end
+
+  @spec face(Card.t()) :: :ace | :jack | :king | :knight | :none
+  def face(%{"rank" => rank}) do
+    case rank do
+      0 -> :ace
+      7 -> :jack
+      8 -> :knight
+      9 -> :king
+      _ -> :none
+    end
   end
 end
--- test/briscola_test.exs
+++ test/briscola_test.exs
@@ -2,7 +2,8 @@ defmodule BriscolaTest do
   use ExUnit.Case
   doctest Briscola
 
-  test "greets the world" do
-    assert Briscola.hello() == :world
+  test "new deck has right number of cards" do
+    # 4 Suits, 10 ranks
+    assert length(Briscola.new_deck().cards) == 4 * 10
   end
 end
