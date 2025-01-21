176f7465bfdd0c6a09512e9c8dba018f408a4cd8
Tim Beck <socaltdb@gmail.com>
2025-01-20 17:18:49 -0800

    init"

---
+++ lib/briscola.ex
@@ -0,0 +1,18 @@
+defmodule Briscola do
+  @moduledoc """
+  Documentation for `Briscola`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Briscola.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/briscola_test.exs
@@ -0,0 +1,8 @@
+defmodule BriscolaTest do
+  use ExUnit.Case
+  doctest Briscola
+
+  test "greets the world" do
+    assert Briscola.hello() == :world
+  end
+end
