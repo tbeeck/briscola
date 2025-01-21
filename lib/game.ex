721de0e1dde19101cc8dcbf3a5ee4d0ec41a39b6
Tim Beck <becktimd@gmail.com>
2025-01-20 22:29:12 -0800

    place in the trick

--- lib/game.ex
+++ lib/game.ex
@@ -9,14 +9,13 @@ defmodule Briscola.Game do
   @spec new(keyword()) :: Briscola.Game.t()
   def new(opts \\ []) do
     players = Keyword.get(opts, :players, 2)
-    hands = Enum.map(1..players, fn _ -> [] end)
 
     {deck, [briscola]} =
       Deck.new()
       |> Deck.shuffle()
       |> Deck.take(1)
 
-    {deck, hands} = deal(deck, hands)
+    {deck, hands} = deal(deck, players)
 
     %Briscola.Game{
       deck: deck,
@@ -28,10 +27,36 @@ defmodule Briscola.Game do
     }
   end
 
-  defp deal(deck, hands) do
-    {new_deck, cards} = Deck.take(deck, @hand_size * length(hands))
+  defp deal(deck, players) do
+    {new_deck, cards} = Deck.take(deck, @hand_size * players)
     hands = Enum.chunk_every(cards, @hand_size)
     {new_deck, hands}
+  end
+
+  def play(game, card_index) do
+    card =
+      Enum.at(game.hands, game.action_on, [])
+      |> Enum.at(card_index)
+
+    if card do
+      hand = Enum.at(game.hands, game.action_on)
+
+      game =
+        %Briscola.Game{
+          game
+          | hands: List.replace_at(game.hands, game.action_on, List.delete(hand, card)),
+            trick: [card | game.trick],
+            action_on: rem(game.action_on + 1, game.players)
+        }
+
+      {:ok, game}
+    else
+      {:error, :invalid_card}
+    end
+  end
+
+  def score_trick(game) when length(game.trick) == game.players do
+    %Briscola.Game{game | trick: []}
   end
 
   @spec trump_suit(t()) :: Briscola.Card.suit()
@@ -39,5 +64,5 @@ defmodule Briscola.Game do
 
   @spec lead_suit(t()) :: Briscola.Card.suit()
   def lead_suit(game) when length(game.trick) == 0, do: nil
-  def lead_suit(game) when length(game.trick) > 0, do: Enum.at(game.trick, 0).suit
+  def lead_suit(game) when length(game.trick) > 0, do: List.last(game.trick).suit
 end
